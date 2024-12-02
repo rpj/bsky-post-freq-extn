41b14940e81dbdfcb677e552fff01bb3e9977649
Ryan Joseph <ryan@electricsheep.co>
2024-12-02 00:19:52 -0800

    Minor cleanup, removing missed unused from project gen

--- tools/post-build.js
+++ tools/post-build.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const distManifest = require('../dist/manifest.json');
-distManifest.content_scripts.map((csEntry, i) => {
+distManifest.content_scripts.forEach((csEntry, i) => {
   if (csEntry.js.some((jsPath) => jsPath.indexOf('assets/content-script-loader.content.js') !== -1)) {
     csEntry.js = csEntry.js.map((jsPath, j) => {
       if (jsPath.indexOf('assets/content-script-loader.content.js') !== -1) {
@@ -10,7 +10,6 @@ distManifest.content_scripts.map((csEntry, i) => {
       return jsPath;
     });
   }
-  return csEntry;
 });
 delete distManifest.web_accessible_resources;
 fs.writeFileSync(path.join(__dirname, '..', 'dist', 'manifest.json'), JSON.stringify(distManifest, null, 2), { flag: 'w+' });
